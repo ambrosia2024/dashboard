223b3ff67826da8e221d68e9b1b18e63d86b1a00
pranavnbapat <pranav.g33k@gmail.com>
2025-07-07 01:14:46 +0200

    pi_testing done

---
+++ pi_test.py
@@ -0,0 +1,10 @@
+from pi_helper import PiSSHClient
+
+# with PiSSHClient() as client:
+#     client.list_files()
+
+# with PiSSHClient() as client:
+#     client.upload_file("my_file.txt")
+
+# with PiSSHClient() as client:
+#     client.download_file("my_file.txt", "downloaded_my_file.txt")
